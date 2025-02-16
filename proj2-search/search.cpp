a07708f8e654e60f1118f374443cf0747b7d7249
unknown <khi.kidman@gmail.com>
2025-02-16 12:50:30 -0600

    Added proj2 starter files

---
+++ proj2-search/search.cpp
@@ -0,0 +1,37 @@
+#include "include/search.h"
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string cleanToken(const string& token) {
+  // TODO student
+  return "";
+}
+
+set<string> gatherTokens(const string& text) {
+  // TODO student
+  return {};
+}
+
+int buildIndex(const string& filename, map<string, set<string>>& index) {
+  // TODO student
+  return 0;
+}
+
+set<string> findQueryMatches(const map<string, set<string>>& index,
+                             const string& sentence) {
+  // TODO student
+  return {};
+}
+
+void searchEngine(const string& filename) {
+  // TODO student
+}
