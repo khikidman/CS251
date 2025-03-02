d23f3f2d0231ac298216182f0736bbbf3652a301
Khi Kidman <khi.kidman@gmail.com>
2025-03-02 14:59:37 -0600

    linkedlist implementation and testing

--- proj3-lists/linkedlist.h
+++ proj3-lists/linkedlist.h
@@ -34,6 +34,8 @@ class LinkedList {
    * Default constructor. Creates an empty `LinkedList`.
    */
   LinkedList() {
+    this->list_size = 0;
+    this->list_front = nullptr;
   }
 
   /**
@@ -41,26 +43,42 @@ class LinkedList {
    * size is 0).
    */
   bool empty() const {
-    return false;
+    return this->list_size == 0;
   }
 
   /**
    * Returns the number of elements in the `LinkedList`.
    */
   size_t size() const {
-    return 0;
+    return this->list_size;
   }
 
   /**
    * Adds the given `T` to the front of the `LinkedList`.
    */
   void push_front(T data) {
+    Node* newNode = new Node(data, list_front);
+    this->list_front = newNode;
+    this->list_size++;
   }
 
   /**
    * Adds the given `T` to the back of the `LinkedList`.
    */
   void push_back(T data) {
+    Node* newNode = new Node(data, nullptr);
+
+    if (this->size() == 0) {
+      this->list_front = newNode;
+    } else {
+      Node* curr = this->list_front;
+      while (curr->next != nullptr) {
+        curr = curr->next;
+      }
+      curr->next = newNode;
+    }
+
+    this->list_size++;
   }
 
   /**
@@ -69,7 +87,18 @@ class LinkedList {
    * If the `LinkedList` is empty, throws a `runtime_error`.
    */
   T pop_front() {
-    return T{};
+    if (this->empty()) {
+      throw out_of_range("pop_front() called on empty list.");
+    }
+
+    Node* oldFront = this->list_front;
+    T oldData = oldFront->data;
+
+    this->list_front = oldFront->next;
+    delete oldFront;
+    this->list_size--;
+    
+    return oldData;
   }
 
   /**
@@ -78,7 +107,30 @@ class LinkedList {
    * If the `LinkedList` is empty, throws a `runtime_error`.
    */
   T pop_back() {
-    return T{};
+    if (this->empty()) {
+      throw out_of_range("pop_front() called on empty list.");
+    }
+
+    Node* curr = this->list_front;
+    Node* prev = nullptr;
+
+    while (curr->next != nullptr) {
+      prev = curr;
+      curr = curr->next;
+    }
+
+    T oldData = curr->data;
+
+    if (prev) {
+      prev->next = nullptr;
+    } else {
+      this->list_front = nullptr;
+    }
+
+    delete curr;
+    this->list_size--;
+
+    return oldData;
   }
 
   /**
@@ -86,12 +138,23 @@ class LinkedList {
    * member variables appropriately.
    */
   void clear() {
+    Node* curr = this->list_front;
+
+    while (curr != nullptr) {
+      Node* next = curr->next;
+      delete curr;
+      curr = next;
+    }
+
+    this->list_front = nullptr;
+    this->list_size = 0;
   }
 
   /**
    * Destructor. Clears all allocated memory.
    */
   ~LinkedList() {
+    this->clear();
   }
 
   /**
@@ -100,7 +163,18 @@ class LinkedList {
    * If the index is invalid, throws `out_of_range`.
    */
   T &at(size_t index) const {
-    return this->list_front->data;
+    if (0 < index && index >= this->size()) {
+      throw out_of_range("at() invalid index access attempt");
+    }
+
+    int i = 0;
+    Node* curr = this->list_front;
+    while (i < index) {
+      curr = curr->next;
+      i++;
+    }
+
+    return curr->data;
   }
 
   /**
@@ -109,6 +183,28 @@ class LinkedList {
    * Must run in O(N) time.
    */
   LinkedList(const LinkedList &other) {
+    this->list_front = nullptr;
+
+    if (other.empty() == nullptr) {
+      return;
+    }
+    
+    Node* curr = other.list_front;
+    Node* prev = nullptr;
+
+    while (curr != nullptr) {
+      Node* newCurr = new Node(curr->data);
+
+      if (prev == nullptr) {
+        this->list_front = newCurr;
+      } else {
+        prev->next = newCurr;
+      }
+
+      prev = newCurr;
+      curr = curr->next;
+      
+    } 
   }
 
   /**
@@ -118,6 +214,33 @@ class LinkedList {
    * Must run in O(N) time.
    */
   LinkedList &operator=(const LinkedList &other) {
+    if (this == &other) {
+      return *this;
+    }
+
+    this->clear();
+
+    if (other.empty()) {
+      return *this;
+    }
+
+    Node* curr = other.list_front;
+    Node* prev = nullptr;
+
+    while (curr != nullptr) {
+      Node* newCurr = new Node(curr->data);
+
+      if (prev == nullptr) {
+        this->list_front = newCurr;
+      } else {
+        prev->next = newCurr;
+      }
+
+      prev = newCurr;
+      curr = curr->next;
+      
+    } 
+
     return *this;
   }
 
@@ -127,7 +250,20 @@ class LinkedList {
    * time.
    */
   string to_string() const {
-    return "";
+    string result = "[";
+    
+    Node* curr = this->list_front;
+
+    while (curr != nullptr) {
+      result += to_string(curr->data);
+      if (curr->next != nullptr) {
+        result += ", ";
+      }
+      curr = curr->next;
+    }
+
+    result += "]";
+    return result;
   }
 
   /**
@@ -135,7 +271,20 @@ class LinkedList {
    * index. If no match is found, returns "-1".
    */
   size_t find(const T &data) {
-    return 0;
+    size_t index = 0;
+
+    Node* curr = this->list_front;
+
+    while (curr != nullptr) {
+      if (curr->data == data) {
+        return index;
+      }
+
+      curr = curr->next;
+      index++;
+    }
+
+    return -1;
   }
 
   /**
@@ -144,6 +293,26 @@ class LinkedList {
    * If the index is invalid, throws `out_of_range`.
    */
   void remove_at(size_t index) {
+    if (index >= this->size()) {
+      throw out_of_range("remove_at() accesses invalid index");
+    }
+
+    Node* curr = this->list_front;
+    Node* prev = nullptr;
+
+    if (index == 0) {
+      this->list_front = curr->next;
+      delete curr;
+      return;
+    }
+
+    for (int i = 0; i < index; i++) {
+      prev = curr;
+      curr = curr->next;
+    }
+
+    prev->next = curr->next;
+    delete curr;
   }
 
   /**
@@ -151,6 +320,19 @@ class LinkedList {
    * the given index. If the index is invalid, throws `out_of_range`.
    */
   void insert_after(size_t index, T data) {
+    if (index >= this->size()) {
+      throw out_of_range("insert_after() accesses invalid index");
+    }
+
+    Node* curr = this->list_front;
+
+    for (int i = 0; i < index; i++) {
+      curr = curr->next;
+    }
+
+    Node* newNode = new Node(data);
+    newNode->next = curr->next;
+    curr->next = newNode;
   }
 
   /**
@@ -161,6 +343,19 @@ class LinkedList {
    * would change the list to `[0, 2, 4]`.
    */
   void remove_every_other() {
+    if (this->size() == 0 || this->size() == 1) {
+      return;
+    }
+    
+    Node* curr = this->list_front;
+    Node* del = nullptr;
+
+    while (curr != nullptr && curr->next != nullptr) {
+      del = curr->next;
+      curr->next = del->next;
+      delete del;
+      curr = curr->next;
+    }
   }
 
   /**
--- proj3-lists/linkedlist_tests.cpp
+++ proj3-lists/linkedlist_tests.cpp
@@ -6,10 +6,140 @@
 using namespace std;
 using namespace testing;
 
-TEST(FakeTest, PleaseDeleteOnceYouWriteSome) {
-  // If no tests exist, GoogleTest fails to compile with a fairly confusing
-  // message. We have this empty test that does nothing to allow an empty
-  // autograder submission to compile and regularly fail. Once you write your
-  // own tests for the project, you can delete this one.
-  EXPECT_THAT(1, Eq(1));
+TEST(LinkedListTest, ConstructorCorrectStartingSize) {
+  LinkedList<int> list;
+  EXPECT_EQ(list.size(), 0);
+  EXPECT_TRUE(list.empty());
+}
+
+TEST(LinkedListTest, EmptyFalseWhenNotEmpty) {
+  LinkedList<int> list;
+  list.push_front(10);
+  EXPECT_FALSE(list.empty());
+}
+
+TEST(LinkedListTest, SizeCorrectNumberOfElements) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.push_back(20);
+  EXPECT_EQ(list.size(), 2);
+}
+
+TEST(LinkedListTest, PushFrontUpdatesSizeAndPushesElement) {
+  LinkedList<int> list;
+  list.push_front(10);
+  EXPECT_EQ(list.size(), 1);
+  EXPECT_EQ(list.at(0), 10);
+}
+
+TEST(LinkedListTest, PushBackUpdatesSizeAndPushesElement) {
+  LinkedList<int> list;
+  list.push_back(10);
+  EXPECT_EQ(list.size(), 1);
+  EXPECT_EQ(list.at(0), 10);
+}
+
+TEST(LinkedListTest, PushFrontKeepsOtherNodes) {
+  LinkedList<int> list;
+  list.push_back(10);
+  list.push_front(20);
+  EXPECT_EQ(list.size(), 2);
+  EXPECT_EQ(list.at(0), 20);
+  EXPECT_EQ(list.at(1), 10);
+}
+
+TEST(LinkedListTest, PushBackOnEmptyList) {
+  LinkedList<int> list;
+  list.push_back(10);
+  EXPECT_EQ(list.size(), 1);
+  EXPECT_EQ(list.at(0), 10);
+}
+
+TEST(LinkedListTest, PopFrontUpdatesSizeAndPopsElement) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.push_back(20);
+  list.pop_front();
+  EXPECT_EQ(list.size(), 1);
+  EXPECT_EQ(list.at(0), 20);
+}
+
+TEST(LinkedListTest, PopBackUpdatesSizeAndPopsElement) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.push_back(20);
+  list.pop_back();
+  EXPECT_EQ(list.size(), 1);
+  EXPECT_EQ(list.at(0), 10);
+}
+
+TEST(LinkedListTest, ClearResetsSizeCorrectly) {
+  LinkedList<int> list;
+  list.push_back(10);
+  list.push_back(20);
+  list.clear();
+  EXPECT_EQ(list.size(), 0);
+  EXPECT_TRUE(list.empty());
+}
+
+TEST(LinkedListTest, DestructorClearsMemoryCorrectly) {
+  LinkedList<int>* list = new LinkedList<int>();
+  list->push_front(10);
+  list->push_back(20);
+  EXPECT_NO_THROW(delete list);  // Ensure no memory leak on destruction
+}
+
+TEST(LinkedListTest, AtThrowsExceptionForInvalidIndex) {
+  LinkedList<int> list;
+  list.push_front(10);
+  EXPECT_THROW(list.at(1), std::out_of_range);
+}
+
+TEST(LinkedListTest, AtReturnsCorrectValue) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.push_back(20);
+  EXPECT_EQ(list.at(0), 10);
+  EXPECT_EQ(list.at(1), 20);
+}
+
+TEST(LinkedListTest, AtChecksBounds) {
+  LinkedList<int> list;
+  list.push_front(10);
+  EXPECT_THROW(list.at(2), std::out_of_range);
+}
+
+TEST(LinkedListTest, PopFrontThrowsExceptionForEmptyList) {
+  LinkedList<int> list;
+  EXPECT_THROW(list.pop_front(), std::out_of_range);
+}
+
+TEST(LinkedListTest, PopBackThrowsExceptionForEmptyList) {
+  LinkedList<int> list;
+  EXPECT_THROW(list.pop_back(), std::out_of_range);
+}
+
+TEST(LinkedListTest, PopBackWithOneElement) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.pop_back();
+  EXPECT_EQ(list.size(), 0);
+  EXPECT_TRUE(list.empty());
+}
+
+TEST(LinkedListTest, ClearMemoryLeakCheck) {
+  LinkedList<int> list;
+  list.push_front(10);
+  list.push_back(20);
+  list.clear();
+  // If no exception or memory issue occurs, the test passes
+  EXPECT_NO_THROW(list.clear());
+}
+
+TEST(LinkedListTest, SizeIsO_N) {
+  LinkedList<int> list;
+  for (int i = 0; i < 1000; ++i) {
+      list.push_back(i);
+  }
+  EXPECT_EQ(list.size(), 1000);
 }
